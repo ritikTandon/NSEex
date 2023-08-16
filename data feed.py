38a8e2d8d598af2af1ab8f9f492bcf6d3d0d1d44
ritikTandon <ritikmukta123@gmail.com>
2023-08-16 20:37:04 +0530

    done finally xD

--- data feed.py
+++ data feed.py
@@ -1,7 +1,5 @@
 import openpyxl as xl
-from openpyxl.styles import Font, Alignment, PatternFill
-import datetime
-from date_variables import date, mnth, yr
+from openpyxl.styles import Font, Alignment
 
 append = 2       # increment this daily. 3 is for 17-AUG-2023
 
@@ -42,39 +40,39 @@ for share in cash_share_names:
     input_row = cash_share_rows[i]+append     # incrementing row values from base row(start row)
 
     # data filling
-    sheet.cell(cash_share_rows[i], 2).value = cashHL_sheet.cell(cashHL_row, 2).value    # high
-    sheet.cell(cash_share_rows[i], 3).value = cashHL_sheet.cell(cashHL_row, 3).value    # low
-    sheet.cell(cash_share_rows[i], 4).value = cashHL_sheet.cell(cashHL_row, 4).value    # close
-    sheet.cell(cash_share_rows[i], 5).value = cashHL_sheet.cell(cashHL_row, 5).value    # LTP
-    sheet.cell(cash_share_rows[i], 6).value = cashHL_sheet.cell(cashHL_row, 6).value    # vol
-    sheet.cell(cash_share_rows[i], 7).value = cashHL_sheet.cell(cashHL_row, 7).value    # 9:25 close
+    sheet.cell(input_row, 2).value = cashHL_sheet.cell(cashHL_row, 2).value    # high
+    sheet.cell(input_row, 3).value = cashHL_sheet.cell(cashHL_row, 3).value    # low
+    sheet.cell(input_row, 4).value = cashHL_sheet.cell(cashHL_row, 4).value    # close
+    sheet.cell(input_row, 5).value = cashHL_sheet.cell(cashHL_row, 5).value    # LTP
+    sheet.cell(input_row, 6).value = cashHL_sheet.cell(cashHL_row, 6).value    # vol
+    sheet.cell(input_row, 7).value = cashHL_sheet.cell(cashHL_row, 7).value    # 9:25 close
 
     # number formatting
     if share not in cash_no_format_list:
-        sheet.cell(cash_share_rows[i], 2).number_format = '0'  # high
-        sheet.cell(cash_share_rows[i], 3).number_format = '0'  # low
-        sheet.cell(cash_share_rows[i], 4).number_format = '0'  # close
-        sheet.cell(cash_share_rows[i], 5).number_format = '0'  # LTP
-        sheet.cell(cash_share_rows[i], 7).number_format = '0'  # 9:25 close
+        sheet.cell(input_row, 2).number_format = '0'  # high
+        sheet.cell(input_row, 3).number_format = '0'  # low
+        sheet.cell(input_row, 4).number_format = '0'  # close
+        sheet.cell(input_row, 5).number_format = '0'  # LTP
+        sheet.cell(input_row, 7).number_format = '0'  # 9:25 close
 
     # style formatting
-    sheet.cell(cash_share_rows[i], 2).font = blue   # high
-    sheet.cell(cash_share_rows[i], 2).alignment = alignment
+    sheet.cell(input_row, 2).font = blue   # high
+    sheet.cell(input_row, 2).alignment = alignment
 
-    sheet.cell(cash_share_rows[i], 3).font = red  # low
-    sheet.cell(cash_share_rows[i], 3).alignment = alignment
+    sheet.cell(input_row, 3).font = red  # low
+    sheet.cell(input_row, 3).alignment = alignment
 
-    sheet.cell(cash_share_rows[i], 4).font = bold  # close
-    sheet.cell(cash_share_rows[i], 4).alignment = alignment
+    sheet.cell(input_row, 4).font = bold  # close
+    sheet.cell(input_row, 4).alignment = alignment
 
-    sheet.cell(cash_share_rows[i], 5).font = bold  # LTP
-    sheet.cell(cash_share_rows[i], 5).alignment = alignment
+    sheet.cell(input_row, 5).font = bold  # LTP
+    sheet.cell(input_row, 5).alignment = alignment
 
-    sheet.cell(cash_share_rows[i], 6).font = bold  # vol
-    sheet.cell(cash_share_rows[i], 6).alignment = alignment
+    sheet.cell(input_row, 6).font = bold  # vol
+    sheet.cell(input_row, 6).alignment = alignment
 
-    sheet.cell(cash_share_rows[i], 7).font = bold  # 9:25 close
-    sheet.cell(cash_share_rows[i], 7).alignment = alignment
+    sheet.cell(input_row, 7).font = bold  # 9:25 close
+    sheet.cell(input_row, 7).alignment = alignment
 
     i += 1
     cashHL_row += 1
@@ -109,39 +107,39 @@ for share in fo_share_names:
     input_row = fo_share_rows[i]+append
 
     # data filling
-    sheet.cell(fo_share_rows[i], 2).value = foHL_sheet.cell(foHL_row, 2).value    # high
-    sheet.cell(fo_share_rows[i], 3).value = foHL_sheet.cell(foHL_row, 3).value    # low
-    sheet.cell(fo_share_rows[i], 4).value = foHL_sheet.cell(foHL_row, 4).value    # close
-    sheet.cell(fo_share_rows[i], 5).value = foHL_sheet.cell(foHL_row, 5).value    # LTP
-    sheet.cell(fo_share_rows[i], 6).value = foHL_sheet.cell(foHL_row, 6).value    # vol
-    sheet.cell(fo_share_rows[i], 7).value = foHL_sheet.cell(foHL_row, 7).value    # 9:25 close
+    sheet.cell(input_row, 2).value = foHL_sheet.cell(foHL_row, 2).value    # high
+    sheet.cell(input_row, 3).value = foHL_sheet.cell(foHL_row, 3).value    # low
+    sheet.cell(input_row, 4).value = foHL_sheet.cell(foHL_row, 4).value    # close
+    sheet.cell(input_row, 5).value = foHL_sheet.cell(foHL_row, 5).value    # LTP
+    sheet.cell(input_row, 6).value = foHL_sheet.cell(foHL_row, 6).value    # vol
+    sheet.cell(input_row, 7).value = foHL_sheet.cell(foHL_row, 7).value    # 9:25 close
 
     # number formatting
     if share not in cash_no_format_list:
-        sheet.cell(fo_share_rows[i], 2).number_format = '0'  # high
-        sheet.cell(fo_share_rows[i], 3).number_format = '0'  # low
-        sheet.cell(fo_share_rows[i], 4).number_format = '0'  # close
-        sheet.cell(fo_share_rows[i], 5).number_format = '0'  # LTP
-        sheet.cell(fo_share_rows[i], 7).number_format = '0'  # 9:25 close
+        sheet.cell(input_row, 2).number_format = '0'  # high
+        sheet.cell(input_row, 3).number_format = '0'  # low
+        sheet.cell(input_row, 4).number_format = '0'  # close
+        sheet.cell(input_row, 5).number_format = '0'  # LTP
+        sheet.cell(input_row, 7).number_format = '0'  # 9:25 close
 
     # style formatting
-    sheet.cell(fo_share_rows[i], 2).font = blue   # high
-    sheet.cell(fo_share_rows[i], 2).alignment = alignment
+    sheet.cell(input_row, 2).font = blue   # high
+    sheet.cell(input_row, 2).alignment = alignment
 
-    sheet.cell(fo_share_rows[i], 3).font = red  # low
-    sheet.cell(fo_share_rows[i], 3).alignment = alignment
+    sheet.cell(input_row, 3).font = red  # low
+    sheet.cell(input_row, 3).alignment = alignment
 
-    sheet.cell(fo_share_rows[i], 4).font = bold  # close
-    sheet.cell(fo_share_rows[i], 4).alignment = alignment
+    sheet.cell(input_row, 4).font = bold  # close
+    sheet.cell(input_row, 4).alignment = alignment
 
-    sheet.cell(fo_share_rows[i], 5).font = bold  # LTP
-    sheet.cell(fo_share_rows[i], 5).alignment = alignment
+    sheet.cell(input_row, 5).font = bold  # LTP
+    sheet.cell(input_row, 5).alignment = alignment
 
-    sheet.cell(fo_share_rows[i], 6).font = bold  # vol
-    sheet.cell(fo_share_rows[i], 6).alignment = alignment
+    sheet.cell(input_row, 6).font = bold  # vol
+    sheet.cell(input_row, 6).alignment = alignment
 
-    sheet.cell(fo_share_rows[i], 7).font = bold  # 9:25 close
-    sheet.cell(fo_share_rows[i], 7).alignment = alignment
+    sheet.cell(input_row, 7).font = bold  # 9:25 close
+    sheet.cell(input_row, 7).alignment = alignment
 
     i += 1
     foHL_row += 1
